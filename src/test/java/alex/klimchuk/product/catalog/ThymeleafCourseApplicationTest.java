4fa34722f152c00a33f3bf618099da7664750c18
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-09 23:41:34 +0100

    add test class

---
+++ src/test/java/alex/klimchuk/product/catalog/ThymeleafCourseApplicationTest.java
@@ -0,0 +1,15 @@
+package alex.klimchuk.product.catalog;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ThymeleafCourseApplicationTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
