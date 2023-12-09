03777ca5e33cb5701627cf64fd06112c039cc99f
Crazy-pro <Crazy.pro.noob@gmail.com>
2023-12-09 03:00:30 +0100

    fix index and product controllers

--- product-catalog/src/main/java/alex/klimchuk/product/catalog/controllers/IndexController.java
+++ product-catalog/src/main/java/alex/klimchuk/product/catalog/controllers/IndexController.java
@@ -1,12 +1,29 @@
 package alex.klimchuk.product.catalog.controllers;
 
+import alex.klimchuk.product.catalog.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/")
-    public String getIndex() {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping({"/", "index"})
+    public String getIndex(Model model) {
+        model.addAttribute("products", productService.listProducts());
         return "index";
+    }
+
+    @RequestMapping("secured")
+    public String secured() {
+        return "secured";
     }
 }
